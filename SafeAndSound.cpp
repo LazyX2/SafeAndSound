b0dfac2f8303dffe4546ae2d29fe34ffa8354bd9
Null and void <94857850+LazyX2@users.noreply.github.com>
2025-12-04 18:41:18 +0200

    Add Safe and sound
    the first problem was that the given size for copying was incorrect
    the second problem was that the copying was weird.

---
+++ SafeAndSound.cpp
@@ -0,0 +1,12 @@
+#include "part1.h"
+#include "part2.h"
+#include <iostream>
+
+int main()
+{
+	part1(); // It should have been given 13 bytes not 12!
+
+	part2();	// debug this after the completion of part1
+
+	return 0;
+}
